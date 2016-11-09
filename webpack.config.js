a71d55e5f8f47b5d704659d72272dc0ac8c38881
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-11-08 21:23:24 -0700

    .gitignore, package.json, webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/jtml.js',
+	devtool: 'source-map',
+	output: {
+		path: './lib',
+		filename: 'jtml.js',
+		library: 'jtml',
+		libraryTarget: 'umd',
+		umdNamedDefine: true
+	},
+	module: {
+		loaders: [
+			{
+				loader: 'babel',
+				test: /\.js$/,
+				exclude: /node_modules/,
+				query: {
+					presets: ['es2015'],
+					plugins: ['babel-plugin-add-module-exports']
+				}
+			}
+		]
+	},
+	plugins: [
+		// new webpack.optimize.UglifyJsPlugin({
+		// 	compress: {
+		// 		warnings: false,
+		// 	},
+		// 	output: {
+		// 		comments: false,
+		// 	},
+		// }),
+	]
+};

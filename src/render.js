d2dc6a883bd42f8069d83c61edc5a532da65dea8
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-11-10 14:57:58 -0700

    Attach events from a `jtml-event` attribute

--- src/render.js
+++ src/render.js
@@ -1,27 +1,46 @@
 import UTILS from './utils';
 import HELPERS from './helpers';
 
+const eventAttributeName = 'jtml-event';
+
 export default class Render {
 	constructor(func) {
 		this.func = func;
 		this.html = '';
 	}
 
-	render(data) {
+	render(data, events) {
 		this.html = this.func(data, UTILS, HELPERS);
+		this.fragment = UTILS.fragmentFromString(this.html);
+
+		Array.from(this.fragment.querySelectorAll(`[${eventAttributeName}]`))
+			.forEach(el => {
+
+				if (events) {
+					el.getAttribute('jtml-event').replace(/\[(.*?)]\((.*?)\)/g, (...args) => {
+						let eventType = args[1];
+						let handlers = args[2].trim().split(/\s+/);
+
+						handlers.forEach(handlers => {
+							el.addEventListener(eventType, events[handlers]);
+						});
+					});
+				}
+
+				el.removeAttribute(eventAttributeName);
+			});
+
 		return this;
 	}
 
 	appendTo(selectorOrNode) {
-		let fragment = UTILS.fragmentFromString(this.html);
-
 		if (UTILS.isDOM(selectorOrNode)) {
-			selectorOrNode.appendChild(fragment);
+			selectorOrNode.appendChild(this.fragment);
 		}
 		else {
 			let element = document.querySelector(selectorOrNode);
 			if (element) {
-				element.appendChild(fragment);
+				element.appendChild(this.fragment);
 			}
 		}
 

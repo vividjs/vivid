b16135ff69388a5a914b66d19776444ac60423c5
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-11-10 11:14:58 -0700

    Rework Compiler to handle code snippets

--- src/compiler.js
+++ src/compiler.js
@@ -1,52 +1,64 @@
-function removeWhiteSpaces(jtml) {
-	return jtml
-		.replace(/\r/g, ' ')
-		.replace(/\n/g, ' ')
-		.replace(/\t/g, ' ');
-}
+import * as CACHE from './cache';
 
-function addWhiteSpaces(jtml) {
-	// return jtml
-	// 	.replace(/__JTML-R__/g, ' ')
-	// 	.replace(/__JTML-N__/g, ' ')
-	// 	.replace(/__JTML-T__/g, ' ');
-}
+const JTML_SPACE = '__JTML-SPACE__';
 
 function customHelper(...replaceMatch) {
-	// console.log('Landed in the custom helper');
 	let command = replaceMatch[1];
-	let customArgs = replaceMatch[2]
-		.replace(/([`'"]).*?[^\\]\1/g, m => m.replace(/\s/g, '__JTML-SPACE__'))
+	let args = replaceMatch[2]
+		.trim()
+		.replace(/([`'"]).*?[^\\]\1/g, m => m.replace(/\s/g, JTML_SPACE))
 		.split(/\s+/)
-		.map(arg => arg.replace(/__JTML-SPACE__/g, ' '));
+		.map(arg => arg.replace(new RegExp(JTML_SPACE, 'g'), ' '));
 
-	// console.log(replaceMatch);
-	// console.log('Command', command);
-	// console.log('Args', customArgs);
-
-	return `', HELPERS.fn['${command}'](${customArgs.join(', ')}), '`;
+	return {command, args};
 }
 
-export default function compiler(jtml) {
-	let functionContent = removeWhiteSpaces(jtml);
 
-	functionContent = `var p = [];
+export function compile(jtml) {
+	// if this template exists in cache, use that instead
+	let preparedTemplate = CACHE.CACHE[jtml];
+
+	if (preparedTemplate) {
+		return preparedTemplate;
+	}
+
+	let functionContent = `var p = [];
 		with (obj) {
 		p.push('${
-		functionContent
-			.replace(/'(?![^{]*})/g, `\\'`)
-			.replace(/{{(\w+) +(.+?) *;* *}}/g, customHelper)
-			.replace(/{{ *(.+?);* *}}/g, `', UTILS.encodeHtml($1), '`)
+		jtml
+			.replace(/[\r\n\t]/g, ' ')
+			// .replace(/'(?![^{]*})/g, `\\'`)
+
+			// Escape all '
+			.replace(/'/g, `\\'`)
+
+			// Un-escape all ' inside of statements
+			.replace(/(?:{{(.*?)}})|(?:{%(.*?)%})/g, match => match.replace(/\\'/g, `'`))
+
+			// Discover helpers
+			.replace(/{{([\w!]+) +(.+?) *;* *}}/g, (...args) => {
+				let helperData = customHelper(...args);
+				return `', HELPERS.fn['${helperData.command}'](${helperData.args.join(', ')}), '`;
+			})
+
+			// Parse output syntax
+			.replace(/{{ *(.+?) *;* *}}/g, `', UTILS.encodeHtml($1), '`)
+
+			// Parse opening code snippet
 			.replace(/{%/g, `');\n`)
+
+			// Parse closing code snippet
 			.replace(/%}/g, `\np.push('`)
 		}');\n}
 		return p.join('')`;
 
-	// functionContent = functionContent
-	// // 	Must preserve ' inside of expressions
-	// 	.replace(/'(?![^{]*})/g, `\\'`)
-	// 	.replace(/{{(\w+) +(.+?) *;* *}}/g, customHelper)
-	// 	.replace(/{{ *(.+?);* *}}/g, "', $1, '");
+	preparedTemplate = new Function('obj', 'UTILS', 'HELPERS', functionContent);
+	CACHE.CACHE[jtml] = preparedTemplate;
+	CACHE.deepCache(jtml, functionContent);
+	return preparedTemplate;
+}
 
-	return new Function('obj', 'UTILS', 'HELPERS', functionContent);
+
+export default {
+	compile,
 }

018915bc1361119d3c258ee45e3208cedd13b5a9
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2016-11-10 11:13:35 -0700

    Cache now uses localStorage

--- src/cache.js
+++ src/cache.js
@@ -1,2 +1,31 @@
-const CACHE = {};
-export default {};
+const localStorageKey = '__JTML_CACHED_TEMPLATES__';
+
+let CACHE = {};
+
+let storedTemplates = JSON.parse(localStorage.getItem(localStorageKey));
+let DEEP_CACHE = storedTemplates || {};
+
+if (storedTemplates) {
+	Object.keys(storedTemplates).forEach(jtml => {
+		CACHE[jtml] = new Function('obj', 'UTILS', 'HELPERS', storedTemplates[jtml]);
+	});
+}
+
+function deepCache(jtml, functionContent) {
+
+	DEEP_CACHE[jtml] = functionContent;
+
+	window.setTimeout(function () {
+		localStorage.setItem(localStorageKey, JSON.stringify(DEEP_CACHE));
+	}, 0);
+}
+
+function clearCache() {
+	localStorage.removeItem(localStorageKey);
+}
+
+export default {
+	CACHE,
+	deepCache,
+	clearCache
+};

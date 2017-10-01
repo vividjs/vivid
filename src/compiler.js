da360b87740dd2b7e28730b20144e0e30ff1ae31
Kevin Okerlund <kevin.m.okerlund@gmail.com>
2017-10-01 11:03:18 -0600

    Correctly named regular expressions in compiler

--- src/compiler.js
+++ src/compiler.js
@@ -1,19 +1,20 @@
 import CACHE from './cache';
 import {BLOCKS} from './blocks';
-import {regexEscape as e, hashCode} from './utils';
 
 const JTML_SPACE = '__JTML-SPACE__';
-const SPACE_REGEX = new RegExp(JTML_SPACE, 'g');
-const NEW_LINES = /[\n\r\t]/g;
-const APOS = /'/g;
-const ESCAPOS = /\\'/g;
-const HELPER = /!\*=\w+ .+\*!/;
 
-const r1 = new RegExp(`(?:!\\*(.*?)\\*!)`, 'g');
-const r2 = new RegExp(`!\\*=([\\w!]+) +(.+?) *;* *\\*!`, 'g');
-const r3 = new RegExp(`!\\*= *(.+?) *;* *\\*!`, 'g');
-const r4 = new RegExp(`!\\*`, 'g');
-const r5 = new RegExp(`\\*!`, 'g');
+/* Compiler Regular Expressions */
+const R_SPACE = new RegExp(JTML_SPACE, 'g');
+const R_NEW_LINES = /[\n\r\t]/g;
+const R_APOSTROPHE = /'/g;
+const R_ESC_APOSTROPHE = /\\'/g;
+const R_HELPER = /!\*=\w+ .+\*!/;
+/* Syntax Resolving Regular Expressions */
+const R_ALL_CODE = new RegExp(`(?:!\\*(.*?)\\*!)`, 'g');
+const R_CUSTOM_HELPER = new RegExp(`!\\*=([\\w!]+) +(.+?) *;* *\\*!`, 'g');
+const R_OUTPUT_BLOCK = new RegExp(`!\\*= *(.+?) *;* *\\*!`, 'g');
+const R_OPENING_SYNTAX = new RegExp(`!\\*`, 'g');
+const R_CLOSING_SYNTAX = new RegExp(`\\*!`, 'g');
 
 
 function customHelper(...replaceMatch) {
@@ -22,7 +23,7 @@ function customHelper(...replaceMatch) {
 		.trim()
 		.replace(/([`'"]).*?[^\\]\1/g, m => m.replace(/\s/g, JTML_SPACE))
 		.split(/\s+/)
-		.map(arg => arg.replace(SPACE_REGEX, ' '));
+		.map(arg => arg.replace(R_SPACE, ' '));
 
 	return {command, args};
 }
@@ -34,8 +35,10 @@ export function compile(jtml) {
 		return CACHE.CACHE[jtml];
 	}
 
-	let replaced = jtml.replace(NEW_LINES, ' ');
+	// Toss all new lines
+	let replaced = jtml.replace(R_NEW_LINES, ' ');
 
+	// Look for custom blocks and replace with regular code
 	if (replaced.indexOf('!#') > -1) {
 		replaced = replaced
 			.replace(/!#(.*?)#!/g, (c, m) => {
@@ -47,32 +50,32 @@ export function compile(jtml) {
 			})
 	}
 
-	// Escape all '
+	// Escape all ' inside of code blocks
 	if (replaced.indexOf(`'`) > -1) {
 		replaced = replaced
-			.replace(APOS, `\\'`)
-			.replace(r1, match => match.replace(ESCAPOS, `'`))
+			.replace(R_APOSTROPHE, `\\'`)
+			.replace(R_ALL_CODE, match => match.replace(R_ESC_APOSTROPHE, `'`));
 	}
 
-	if (HELPER.test(replaced)) {
-		replaced = replaced.replace(r2, (...args) => {
+	// Look for custom helpers
+	if (R_HELPER.test(replaced)) {
+		replaced = replaced.replace(R_CUSTOM_HELPER, (...args) => {
 			let helperData = customHelper(...args);
 			return `', HELPERS.fn['${helperData.command}'](${helperData.args.join(', ')}), '`;
 		});
 	}
 
 	// Parse output syntax
-	replaced = replaced.replace(r3, `', UTILS.encodeHtml($1), '`);
+	replaced = replaced.replace(R_OUTPUT_BLOCK, `', UTILS.encodeHtml($1), '`);
 
 	// Parse opening code snippet
-	replaced = replaced.replace(r4, `');`);
+	replaced = replaced.replace(R_OPENING_SYNTAX, `');`);
 
 	// Parse closing code snippet
-	replaced = replaced.replace(r5, `p.push('`);
+	replaced = replaced.replace(R_CLOSING_SYNTAX, `p.push('`);
 
 
 	let functionContent = `var p = [];p.push('${replaced}');\nreturn p.join('')`;
-
 	let func = new Function('UTILS', 'HELPERS', functionContent);
 	CACHE.CACHE[jtml] = func;
 	return func;

2037d384035b2197e135414f2f0db378035113c5
Kevin Okerlund <kevinokerlund@users.noreply.github.com>
2019-01-28 12:38:50 -0700

    Commented code ideas for staring out

--- src/config.js
+++ src/config.js
@@ -6,3 +6,15 @@ export let syntax = {
 	openingBlockHelper: '{#',
 	closingBlockHelper: '#}',
 };
+
+
+// import {getQueryParams} from './utils';
+
+// const params = getQueryParams();
+// const JTML_IN_URL = !!params.jtml;
+
+// export const IS_DEV_MODE = JTML_IN_URL && params.jtml.includes('dev');
+// export const VERBOSE_MODE = JTML_IN_URL && params.jtml.includes('verbose');
+
+// console.log('DEV MODE:', IS_DEV_MODE);
+// console.log('VERBOSE MODE:', VERBOSE_MODE);
